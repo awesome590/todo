63e0a0d82b3a5f896398c10f9d3577850680827b
Reece Peardon <peardon.reece@gmail.com>
2026-02-13 22:56:02 -0800

    started on making card for list items

---
+++ src/components/ListCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ListCard() {
+    return (
+            <div className="list_container">
+                <ul className="list">
+                    <li className="card">
+                        <h2 className="card_title">Laundry</h2>
+                        <p className="card_description">Use different detergent</p>
+                    </li>
+                </ul>
+            </div>
+    )
+}
+
+export default ListCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ListCard from "../components/ListCard.jsx";
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="home">
+            <ListCard/>
+        </div>
     )
 }
 
